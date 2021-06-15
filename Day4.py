2511c167f53f6fee80a54798a386d2c8719105c5
Puneet Singh <71906468+its-puneet@users.noreply.github.com>
2021-06-15 14:47:28 +0530

    Add files via upload

---
+++ Day4.py
@@ -0,0 +1,40 @@
+""" 1)Create three variables (a,b,c) to same value of any integer &
+do the following
+a) Divide a by 10
+b) Multiply b by 50
+c) Add c value by 60 """
+
+a=b=c=50
+print(a)
+print(b)
+print(c)
+
+print()
+
+x=a/10
+print(x)
+
+y=b*50
+print(y)
+
+z=c+60
+print(z)
+
+print()
+
+""" 2)Create a String variable of 5 characters and replace the 3rd
+character with G """
+
+s="GITHUB"
+q=s.replace(s[2], "G")
+print(s)
+print(q)
+
+print()
+""" 3)Create two values (a,b) of int,float data type & convert the
+vise versa, Hint : convert a from int to float datatype & b from
+float to int datatype  """
+
+(a,b)=(10,15.0)
+print((a,b))
+print((float(a),int(b)))
